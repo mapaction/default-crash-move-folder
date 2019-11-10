57073f52f0fe0bcb99fafca4e546f4ff642406ba
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-11-10 00:03:29 +0000

    Update deploy-via-ftp-server.sh

--- deploy-via-ftp-server.sh
+++ deploy-via-ftp-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo $GO_SCM_DEFAULT_CMF_PR_BRANCH
-if [ -n $GO_SCM_DEFAULT_CMF_PR_BRANCH] && [ "mapactionmaster" = $GO_SCM_DEFAULT_CMF_PR_BRANCH ]
+# echo $GO_SCM_DEFAULT_CMF_PR_BRANCH
+if [ -n $GO_SCM_DEFAULT_CMF_PR_BRANCH] && [ "mapaction:master" = $GO_SCM_DEFAULT_CMF_PR_BRANCH ]
 then
   echo deploy from master
   lftp -f default-cmf/deploy-via-ftp-server.lftp.txt
