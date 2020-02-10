3c918dc5f80196a85aefcf8898d3fbd924fdec6f
Dconnaghan <darrenconnaghan@geosmartinfo.co.uk>
2020-02-10 10:54:59 +0000

    Anchor Point Resolution
    In order to assist Ant with his conversion of ArcGIS templates to QGIS, all elements had their anchor points changed to be Bottom Left
    All five templates now have the same tags (their was some discrepency between all of them)
    mapaction_300px tag has been changed to mapation_logo

--- 20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/mapaction_mapelementlocations.py
+++ 20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/mapaction_mapelementlocations.py
@@ -6,27 +6,55 @@ from shutil import copyfile
 import arcpy
 from arcpy import env
 
-# Setting up variables
-root = "C:/ff_GitHub/default-crash-move-folder/20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/arcgis_10_6/"
-arcpy.env.workspace = root
-mxdList = arcpy.ListFiles("*.mxd")
+import os
+sep = os.path.sep
 
+# gets location of this python script which is at root of github
+rootauto = os.path.abspath(os.getcwd())
+# Setting up variables
+##root = rootauto + r"\default-crash-move-folder\20YYiso3nn\GIS\3_Mapping\32_MXD_Templates\arcgis_10_6"
+root = rootauto + r"\arcgis_10_6"
+
+# setting the workspace for the arcpy calls
+arcpy.env.workspace = root
+# lists all the mxd's in the relevant folder
+mxdList = arcpy.ListFiles("*.mxd")
+    
 # loop through the elements and re5cord the values
-mapframedetails = []
+print "Template,Element,PositionX,PositionY,Height,Width"
+##mapframedetails = []
 for mxdfile in mxdList:
     mapframedetails.append(mxdfile)
-    print mxdfile
-    print "element,position x,position y,height,width"
-    mxd = arcpy.mapping.MapDocument(root + mxdfile)
+    mxd = arcpy.mapping.MapDocument(root + sep + mxdfile)
     df = arcpy.mapping.ListDataFrames(mxd, "*")
+    # run through the elements within the map and print
     for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
+        print str(mxdfile) + ",",
         print str(elem.name) + ",",
+##        print str(elem.textSize) + ",",
         print str(elem.elementPositionX) + ",",
         print str(elem.elementPositionY) + ",",
         print str(elem.elementHeight) + ",",
         print str(elem.elementWidth)
     print "\n"
 
+    # Saving 10.2 and 9.3 copies of mxd's in correct location and name
+    mxd10_1 = root[:-4] + "10_2" + sep + mxdfile[:7] + "10_2" + mxdfile[11:]
+    mxd.saveACopy(mxd10_1, "10.1")
+    mxd9_3 = root[:-4] + "9_3" + sep + mxdfile[:7] + "9_3" + mxdfile[11:]
+    mxd.saveACopy(mxd9_3, "9.3")
+
+    # want to generate a list / array which can be sorted alphabetically
+    # help please
+##        element = element +  mapfile + ","
+##        element = element +  str(elem.name) + ",",
+##        element = element +  str(elem.elementPositionX) + "\n"#",",
+##        element = element +  str(elem.elementPositionY) + ",",
+##        element = element +  str(elem.elementHeight) + ",",
+##        element = element +  str(elem.elementWidth) + "\n"
+        
+##        mapframedetails.append(element)
+##        mapframedetails.append(",")
 ##        mapframedetails.append(elem.name)
 ##        mapframedetails.append(",")
 ##        mapframedetails.append(elem.elementHeight)
@@ -37,5 +65,6 @@ for mxdfile in mxdList:
 ##        mapframedetails.append(",")
 ##        mapframedetails.append(elem.elementPositionY)
 ##        mapframedetails.append("\n")
+##    mapframedetails.sort()
 ##    print mapframedetails
 ##    print "\n"
