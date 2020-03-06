6cc4362ecaf0456dc99a14c906a7cecb9831352a
dconnaghanx <56038963+dconnaghanx@users.noreply.github.com>
2020-03-06 16:45:12 +0000

    Corrected json files which were incorrect

--- 20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/mapaction_mapelementlocations.py
+++ 20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/mapaction_mapelementlocations.py
@@ -6,36 +6,41 @@ from shutil import copyfile
 import arcpy
 from arcpy import env
 
+import os
+rootauto = os.path.abspath(os.getcwd())
+##rootauto = os.path.dirname(os.path.abspath(__mapaction_mapelementlocations.py__))
+print rootauto
 # Setting up variables
-root = "C:/ff_GitHub/default-crash-move-folder/20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/arcgis_10_6/"
-arcpy.env.workspace = root
-mxdList = arcpy.ListFiles("*.mxd")
-
-# loop through the elements and re5cord the values
-mapframedetails = []
-for mxdfile in mxdList:
-    mapframedetails.append(mxdfile)
-    print mxdfile
-    print "element,position x,position y,height,width"
-    mxd = arcpy.mapping.MapDocument(root + mxdfile)
-    df = arcpy.mapping.ListDataFrames(mxd, "*")
-    for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
-        print str(elem.name) + ",",
-        print str(elem.elementPositionX) + ",",
-        print str(elem.elementPositionY) + ",",
-        print str(elem.elementHeight) + ",",
-        print str(elem.elementWidth)
-    print "\n"
-
-##        mapframedetails.append(elem.name)
-##        mapframedetails.append(",")
-##        mapframedetails.append(elem.elementHeight)
-##        mapframedetails.append(",")
-##        mapframedetails.append(elem.elementWidth)
-##        mapframedetails.append(",")
-##        mapframedetails.append(elem.elementPositionX)
-##        mapframedetails.append(",")
-##        mapframedetails.append(elem.elementPositionY)
-##        mapframedetails.append("\n")
-##    print mapframedetails
+##root = "C:/ff_GitHub/default-crash-move-folder/20YYiso3nn/GIS/3_Mapping/32_MXD_Templates/arcgis_10_6/"
+##arcpy.env.workspace = root
+##mxdList = arcpy.ListFiles("*.mxd")
+##
+### loop through the elements and re5cord the values
+##print "Template,Element,PositionX,PositionY,Height,Width"
+##mapframedetails = []
+##for mxdfile in mxdList:
+##    mapframedetails.append(mxdfile)
+##    mxd = arcpy.mapping.MapDocument(root + mxdfile)
+##    df = arcpy.mapping.ListDataFrames(mxd, "*")
+##    for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
+##        print str(mxdfile) + ",",
+##        print str(elem.name) + ",",
+####        print str(elem.textSize) + ",",
+##        print str(elem.elementPositionX) + ",",
+##        print str(elem.elementPositionY) + ",",
+##        print str(elem.elementHeight) + ",",
+##        print str(elem.elementWidth)
 ##    print "\n"
+##
+####        mapframedetails.append(elem.name)
+####        mapframedetails.append(",")
+####        mapframedetails.append(elem.elementHeight)
+####        mapframedetails.append(",")
+####        mapframedetails.append(elem.elementWidth)
+####        mapframedetails.append(",")
+####        mapframedetails.append(elem.elementPositionX)
+####        mapframedetails.append(",")
+####        mapframedetails.append(elem.elementPositionY)
+####        mapframedetails.append("\n")
+####    print mapframedetails
+####    print "\n"
