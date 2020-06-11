5d471b241054663b310ba2b2415f7f40fd94ca19
dconnaghanx <56038963+dconnaghanx@users.noreply.github.com>
2020-06-11 17:12:52 +0100

    Final final new templates
    Changed font to Eurostyle
    Matched gaps throughout
    Changed line work to be MapAction colour
    Re-arranged order of elements so overview appears above / left of sub-title so if no sub-title, doesn't look that bad
    Moved legend to marginalia area on Portrait
    Added map_name (eventually remove mxd_name

---
+++ 20YYiso3nn/GIS/3_Mapping/32_Templates/mapaction_mapelementlocations.py
@@ -0,0 +1,71 @@
+""" This scripts brings together all data and maps for the report """
+
+import os
+import sys
+import string
+import shutil
+from shutil import copyfile
+import datetime
+#from datetime import date
+today = str(datetime.datetime.now())
+#print today[0:10]
+
+import arcpy
+from arcpy import env
+
+sep = os.path.sep
+
+# gets location of this python script which is at root of github
+rootauto = os.getcwd()
+print rootauto
+mapfile = rootauto + sep + "template_positions_" + today[0:10] + ".txt"
+print mapfile
+# Setting up variables
+arcpy.env.workspace = rootauto
+# lists all the mxd's in the relevant folder
+mxdList = arcpy.ListFiles("*.mxd")
+element = ""
+
+# loop through the elements and record the values
+element = "Type,ElementName,PositionX,PositionY,Height,Width,FontSize,TextValue\n"
+
+for mxdfile in mxdList:
+    mxd = arcpy.mapping.MapDocument(rootauto + sep + mxdfile)
+    elemmxd = mxdfile
+    element = element + "\n" + elemmxd 
+    df = arcpy.mapping.ListDataFrames(mxd, "*")
+    for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
+# just printing to the screen
+        elemname = str(elem.name)
+        elemtype = str(elem.type)
+        elemposx = str(elem.elementPositionX)
+        elemposy = str(elem.elementPositionY)
+        elemheight = str(elem.elementHeight)
+        elemwidth = str(elem.elementWidth)
+        element = element + "\n" + elemname + "," + elemtype + "," + elemposx + "," + elemposy + ","
+        element = element + elemheight + "," + elemwidth
+
+        if elemtype == "TEXT_ELEMENT":
+            elemfontsize = str(elem.fontSize)
+            elemtext = str(elem.text)
+            element = element + "," + elemfontsize + "," + elemtext
+
+    element = element + "\n"
+
+    # Saving 10.1 and 9.3 copies of mxd's in correct location and name
+    mxd10_1 = rootauto + sep + "arcgis_10_1" + sep + mxdfile[:7] + "10_1" + mxdfile[11:]
+    mxd.saveACopy(mxd10_1, "10.1")
+    mxd9_3 = rootauto + sep + "arcgis_9_3" + sep + mxdfile[:7] + "9_3" + mxdfile[11:]
+    mxd.saveACopy(mxd9_3, "9.3")
+
+    finaljpg = rootauto + sep + mxdfile[:-4] + ".jpg"
+    arcpy.mapping.ExportToJPEG(mxd, finaljpg, resolution = 250)
+
+# open template_positions_20200610.txt
+file_object = open(mapfile, "w+")
+# add 'elememt' variable to file
+file_object.write(element)
+file_object.close()
+# save template_positions_20200610.txt
+
+#print element + "\n"
