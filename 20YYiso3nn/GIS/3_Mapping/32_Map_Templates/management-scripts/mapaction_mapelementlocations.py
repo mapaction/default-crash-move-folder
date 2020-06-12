8f2a6b4bb24003a5691b581b39fd50e3f54a6568
dconnaghanx <56038963+dconnaghanx@users.noreply.github.com>
2020-06-12 11:44:40 +0100

    Update mapaction_mapelementlocations.py
    Improved to generate text file with element values etc

--- 20YYiso3nn/GIS/3_Mapping/32_Map_Templates/management-scripts/mapaction_mapelementlocations.py
+++ 20YYiso3nn/GIS/3_Mapping/32_Map_Templates/management-scripts/mapaction_mapelementlocations.py
@@ -1,49 +1,71 @@
 """ This scripts brings together all data and maps for the report """
 
-import sys
-sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib\\site-packages")
-sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\arcpy')
-sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\ArcToolbox\Scripts')
-sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\bin')
-sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib")
-import arcpy
-from arcpy import env
-
 import os
+import sys
 import string
 import shutil
 from shutil import copyfile
+import datetime
+#from datetime import date
+today = str(datetime.datetime.now())
+#print today[0:10]
 
-import os
+import arcpy
+from arcpy import env
+
 sep = os.path.sep
 
 # gets location of this python script which is at root of github
-rootauto = os.path.abspath(os.getcwd())
+rootauto = os.getcwd()
+print rootauto
+mapfile = rootauto + sep + "template_positions_" + today[0:10] + ".txt"
+print mapfile
 # Setting up variables
-root = rootauto + r"\arcgis_10_6"
-
-# setting the workspace for the arcpy calls
-arcpy.env.workspace = root
+arcpy.env.workspace = rootauto
 # lists all the mxd's in the relevant folder
 mxdList = arcpy.ListFiles("*.mxd")
-    
-# loop through the elements and re5cord the values
-print "Template,Element,PositionX,PositionY,Height,Width"
-for mxdfile in mxdList:
-    mxd = arcpy.mapping.MapDocument(root + sep + mxdfile)
-    df = arcpy.mapping.ListDataFrames(mxd, "*")
-    # run through the elements within the map and print
-    for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
-        print str(mxdfile) + ",",
-        print str(elem.name) + ",",
-        print str(elem.elementPositionX) + ",",
-        print str(elem.elementPositionY) + ",",
-        print str(elem.elementHeight) + ",",
-        print str(elem.elementWidth)
-    print "\n"
+element = ""
 
-    # Saving 10.2 and 9.3 copies of mxd's in correct location and name
-    mxd10_1 = root[:-4] + "10_2" + sep + mxdfile[:7] + "10_2" + mxdfile[11:]
+# loop through the elements and record the values
+element = "Type,ElementName,PositionX,PositionY,Height,Width,FontSize,TextValue\n"
+
+for mxdfile in mxdList:
+    mxd = arcpy.mapping.MapDocument(rootauto + sep + mxdfile)
+    elemmxd = mxdfile
+    element = element + "\n" + elemmxd 
+    df = arcpy.mapping.ListDataFrames(mxd, "*")
+    for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
+# just printing to the screen
+        elemname = str(elem.name)
+        elemtype = str(elem.type)
+        elemposx = str(elem.elementPositionX)
+        elemposy = str(elem.elementPositionY)
+        elemheight = str(elem.elementHeight)
+        elemwidth = str(elem.elementWidth)
+        element = element + "\n" + elemname + "," + elemtype + "," + elemposx + "," + elemposy + ","
+        element = element + elemheight + "," + elemwidth
+
+        if elemtype == "TEXT_ELEMENT":
+            elemfontsize = str(elem.fontSize)
+            elemtext = str(elem.text)
+            element = element + "," + elemfontsize + "," + elemtext
+
+    element = element + "\n"
+
+    # Saving 10.1 and 9.3 copies of mxd's in correct location and name
+    mxd10_1 = rootauto + sep + "arcgis_10_1" + sep + mxdfile[:7] + "10_1" + mxdfile[11:]
     mxd.saveACopy(mxd10_1, "10.1")
-    mxd9_3 = root[:-4] + "9_3" + sep + mxdfile[:7] + "9_3" + mxdfile[11:]
+    mxd9_3 = rootauto + sep + "arcgis_9_3" + sep + mxdfile[:7] + "9_3" + mxdfile[11:]
     mxd.saveACopy(mxd9_3, "9.3")
+
+    finaljpg = rootauto + sep + mxdfile[:-4] + ".jpg"
+    arcpy.mapping.ExportToJPEG(mxd, finaljpg, resolution = 250)
+
+# open template_positions_20200610.txt
+file_object = open(mapfile, "w+")
+# add 'elememt' variable to file
+file_object.write(element)
+file_object.close()
+# save template_positions_20200610.txt
+
+#print element + "\n"
