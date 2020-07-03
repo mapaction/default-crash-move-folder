9e258bf8ef23245f2389e11f37b7a348cce415a6
dconnaghanx <56038963+dconnaghanx@users.noreply.github.com>
2020-07-03 18:19:23 +0100

    Better positioning of text files (#39)
    * Better positioning of text files
    
    Listing positions in alphabetical order
    Updated pagx (pro templates)
    
    * Increasing box size
    
    If non MapAction laptop, defaults to Arial, I've made the boes large enough to deal with this

--- 20YYiso3nn/GIS/3_Mapping/32_Map_Templates/management-scripts/mapaction_mapelementlocations-old.py
+++ 20YYiso3nn/GIS/3_Mapping/32_Map_Templates/management-scripts/mapaction_mapelementlocations-old.py
@@ -1,11 +1,11 @@
 """ This scripts brings together all data and maps for the report """
 
 import sys
-#sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib\\site-packages")
-#sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\arcpy')
-#sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\ArcToolbox\Scripts')
-#sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\bin')
-#sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib")
+sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib\\site-packages")
+sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\arcpy')
+sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\ArcToolbox\Scripts')
+sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\bin')
+sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib")
 import arcpy
 from arcpy import env
 
@@ -32,18 +32,17 @@ arcpy.env.workspace = rootauto[:-19]
 # lists all the mxd's in the relevant folder
 mxdList = arcpy.ListFiles("*.mxd")
 
-# final 'element' variable
-element_final = ''
+# loop through the elements and record the values
+element = "Type,ElementName,PositionX,PositionY,Height,Width,FontSize,TextValue\n"
 
 for mxdfile in mxdList:
     mxd = arcpy.mapping.MapDocument(rootauto[:-19] + sep + mxdfile)
     elemmxd = mxdfile
-    element = elemmxd
+    element = element + "\n" + elemmxd 
     df = arcpy.mapping.ListDataFrames(mxd, "*")
-
     # run through the elements within the map and print
     for elem in arcpy.mapping.ListLayoutElements(mxd, ""):
-    # storing in variable to be sorted
+    # just printing to the screen
         elemname = str(elem.name)
         elemtype = str(elem.type)
         elemposx = str(elem.elementPositionX)
@@ -51,8 +50,7 @@ for mxdfile in mxdList:
         elemheight = str(elem.elementHeight)
         elemwidth = str(elem.elementWidth)
         element = element + "\n" + elemname + "," + elemtype + "," + elemposx + "," + elemposy + ","
-        element = element + elemheight + "," + elemwidth    
-
+        element = element + elemheight + "," + elemwidth
 
         if elemtype == "TEXT_ELEMENT":
             elemfontsize = str(elem.fontSize)
@@ -60,18 +58,7 @@ for mxdfile in mxdList:
             element = element + "," + elemfontsize + "," + elemtext
 
     element = element + "\n"
-    # convert to string in case of unicode issues
-    element = str(element)
-    # convert to List and order alphabetically
-    testLst = element.split("\n")
-    # avoid treatment of capital letters as lower values when ordering alphabetically
-    testLst.sort(key=str.lower)
 
-    # create alphabetic element
-    element_alphabetic = "\n".join(testLst)
-    element_final = element_final + element_alphabetic + "\n"
-
-    
     # Saving 10.1, 10.0 and 9.3 copies of mxd's in correct location and name
     mxd10_1 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_10_1" + sep + mxdfile[:7] + "10_1" + mxdfile[11:]
     mxd.saveACopy(mxd10_1, "10.1")
@@ -83,9 +70,9 @@ for mxdfile in mxdList:
     finaljpg = rootauto[:-19] + sep + "example-outputs" + sep + mxdfile[:-4] + ".jpg"
     arcpy.mapping.ExportToJPEG(mxd, finaljpg, resolution = 300)
 
-# open template_positions_yyyy-mm-dd.txt
+# open template_positions_yyyymmdd.txt
 file_object = open(mapfile, "w+")
-# add 'elememt' variable to start of the file
-element_final = "Type,ElementName,PositionX,PositionY,Height,Width,FontSize,TextValue\n" + element_final
-file_object.write(element_final)
+# add 'elememt' variable to file
+file_object.write(element)
 file_object.close()
+# save template_positions_20200610.txt
