2c773f6478d4c0a023caed27cf17b54eb2f9ce41
dconnaghanx <56038963+dconnaghanx@users.noreply.github.com>
2020-07-17 14:19:28 +0100

    Ants Changes Applied

--- 20YYiso3nn/GIS/3_Mapping/32_Map_Templates/management-scripts/mapaction_mapelementlocations_DC.py
+++ 20YYiso3nn/GIS/3_Mapping/32_Map_Templates/management-scripts/mapaction_mapelementlocations_DC.py
@@ -1,11 +1,11 @@
 """ This scripts brings together all data and maps for the report """
 
 import sys
-sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib\\site-packages")
-sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\arcpy')
-sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\ArcToolbox\Scripts')
-sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\bin')
-sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib")
+#sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib\\site-packages")
+#sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\arcpy')
+#sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\ArcToolbox\Scripts')
+#sys.path.append(r'C:\Program Files (x86)\ArcGIS\Desktop10.6\bin')
+#sys.path.append("C:\\py27arcgis106\\ArcGIS10.6\\Lib")
 import arcpy
 from arcpy import env
 
@@ -15,7 +15,9 @@ import string
 import shutil
 from shutil import copyfile
 import datetime
+#from datetime import date
 today = str(datetime.datetime.now())
+#print today[0:10]
 
 import arcpy
 from arcpy import env
@@ -25,9 +27,11 @@ sep = os.path.sep
 
 # gets location of this python script which is at root of github
 rootauto = os.getcwd()
+print rootauto
 mapfile = rootauto + sep + "template_positions_" + today[0:10] + ".txt"
-
-# setting up our workspace
+print mapfile
+#print "\nrootauto: " + rootauto[:-19] + "\n"
+# setting up are workspace
 arcpy.env.workspace = rootauto[:-19]
 # lists all the mxd's in the relevant folder
 mxdList = arcpy.ListFiles("*.mxd")
@@ -60,15 +64,15 @@ for mxdfile in mxdList:
     element = element + "\n"
 
     # Saving 10.1, 10.0 and 9.3 copies of mxd's in correct location and name
-    mxd10_1 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_10_1" + sep + mxdfile[:7] + "10_1" + mxdfile[11:]
-    mxd.saveACopy(mxd10_1, "10.1")
-    mxd10_0 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_10_0" + sep + mxdfile[:7] + "10_0" + mxdfile[11:]
-    mxd.saveACopy(mxd10_0, "10.0")
-    mxd9_3 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_9_3" + sep + mxdfile[:7] + "9_3" + mxdfile[11:]
-    mxd.saveACopy(mxd9_3, "9.3")
+#    mxd10_1 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_10_1" + sep + mxdfile[:7] + "10_1" + mxdfile[11:]
+#    mxd.saveACopy(mxd10_1, "10.1")
+#    mxd10_0 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_10_0" + sep + mxdfile[:7] + "10_0" + mxdfile[11:]
+#    mxd.saveACopy(mxd10_0, "10.0")
+#    mxd9_3 = rootauto[:-19] + sep + "previous_versions" + sep + "arcmap_9_3" + sep + mxdfile[:7] + "9_3" + mxdfile[11:]
+#    mxd.saveACopy(mxd9_3, "9.3")
 
     finaljpg = rootauto[:-19] + sep + "example-outputs" + sep + mxdfile[:-4] + ".jpg"
-    arcpy.mapping.ExportToJPEG(mxd, finaljpg, resolution = 300)
+ #   arcpy.mapping.ExportToJPEG(mxd, finaljpg, resolution = 300)
 
 # open template_positions_yyyymmdd.txt
 file_object = open(mapfile, "w+")
